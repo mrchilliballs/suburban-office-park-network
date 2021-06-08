0f932906cc090a4e95f0b1a12f906a53ec46b569
WildSir <83824803+mrchilliballs@users.noreply.github.com>
2021-06-07 22:53:24 -0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-app.get("/", (req, res)=>{
-  res.send("hello")
-})
-app.listen(process.env.PORT, ()=>{
-  console.log("listening yo port 3001")
-})
