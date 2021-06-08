830f675c0f71b002330c5fc1bf34511f46a111a3
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2021-06-07 22:39:33 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html>
   <head>
     <title>Suburban Office Park Network- A network of cool suburban websites from office buildings.</title>
+    
+    <link href="https://codesalvageon.github.io/magichat/styles/index.css" rel="stylesheet" type="text/css">
   </head>
   <body>
   </body>
