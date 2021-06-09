6b8a273cef38c4fad4719e15e721de9e61887d74
WildSir <83824803+mrchilliballs@users.noreply.github.com>
2021-06-09 13:26:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
     <title>Suburban Office Park Network- A network of cool suburban websites from office buildings.</title>
     
     <link href="https://codesalvageon.github.io/magichat/styles/index.css" rel="stylesheet" type="text/css">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BLSB7LGJMR"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BLSB7LGJMR');
+</script>
   </head>
   <body>
   </body>
